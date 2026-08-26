54a9e1debd6682446e1fedd74b7abed0ff541b13
Carlos O'Ryan <coryan@google.com>
2026-08-26 18:08:02 +0000

    cleanup: rename `GoogleCloudWkt` (#543)
    Rename `GoogleCloudWkt` to `GoogleCloudWKT` because the convention in
    Swift is to use all caps for acronyms, in this case `WKT` == Well-Known
    Types.

--- Sources/GoogleAppsScriptTypeCalendar/CalendarAddOnManifest.swift
+++ Sources/GoogleAppsScriptTypeCalendar/CalendarAddOnManifest.swift
@@ -16,10 +16,10 @@
 
 import Foundation
 import GoogleAppsScriptType
-@_spi(GoogleCloudInternal) import GoogleCloudWkt
+@_spi(GoogleCloudInternal) import GoogleCloudWKT
 
 /// Calendar add-on manifest.
-public struct CalendarAddOnManifest: Codable, Equatable, GoogleCloudWkt._AnyPackable,
+public struct CalendarAddOnManifest: Codable, Equatable, GoogleCloudWKT._AnyPackable,
   Sendable
 {
   /// Defines an endpoint that will be executed contexts that don't
@@ -191,10 +191,10 @@ public struct CalendarAddOnManifest: Codable, Equatable, GoogleCloudWkt._AnyPack
   public static var _anyTypeUrl: Swift.String {
     return "type.googleapis.com/google.apps.script.type.calendar.CalendarAddOnManifest"
   }
-  public init(fromAny any: GoogleCloudWkt.`Any`) throws {
-    self = try GoogleCloudWkt._slowAnyDeserialize(Self.self, from: any)
+  public init(fromAny any: GoogleCloudWKT.`Any`) throws {
+    self = try GoogleCloudWKT._slowAnyDeserialize(Self.self, from: any)
   }
-  public func _pack() throws -> GoogleCloudWkt.Struct {
-    return try GoogleCloudWkt._slowAnySerialize(message: self)
+  public func _pack() throws -> GoogleCloudWKT.Struct {
+    return try GoogleCloudWKT._slowAnySerialize(message: self)
   }
 }
--- Sources/GoogleAppsScriptTypeCalendar/CalendarExtensionPoint.swift
+++ Sources/GoogleAppsScriptTypeCalendar/CalendarExtensionPoint.swift
@@ -15,10 +15,10 @@
 // limitations under the License.
 
 import Foundation
-@_spi(GoogleCloudInternal) import GoogleCloudWkt
+@_spi(GoogleCloudInternal) import GoogleCloudWKT
 
 /// Common format for declaring a calendar add-on's triggers.
-public struct CalendarExtensionPoint: Codable, Equatable, GoogleCloudWkt._AnyPackable,
+public struct CalendarExtensionPoint: Codable, Equatable, GoogleCloudWKT._AnyPackable,
   Sendable
 {
   /// Required. The endpoint to execute when this extension point is
@@ -44,10 +44,10 @@ public struct CalendarExtensionPoint: Codable, Equatable, GoogleCloudWkt._AnyPac
   public static var _anyTypeUrl: Swift.String {
     return "type.googleapis.com/google.apps.script.type.calendar.CalendarExtensionPoint"
   }
-  public init(fromAny any: GoogleCloudWkt.`Any`) throws {
-    self = try GoogleCloudWkt._slowAnyDeserialize(Self.self, from: any)
+  public init(fromAny any: GoogleCloudWKT.`Any`) throws {
+    self = try GoogleCloudWKT._slowAnyDeserialize(Self.self, from: any)
   }
-  public func _pack() throws -> GoogleCloudWkt.Struct {
-    return try GoogleCloudWkt._slowAnySerialize(message: self)
+  public func _pack() throws -> GoogleCloudWKT.Struct {
+    return try GoogleCloudWKT._slowAnySerialize(message: self)
   }
 }
