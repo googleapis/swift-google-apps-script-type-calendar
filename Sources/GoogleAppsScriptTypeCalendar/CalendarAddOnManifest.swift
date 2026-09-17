6153a87c4542a3cd959c98088deb5e87eb4e8643
Jeff Ching <chingor@google.com>
2026-09-17 11:23:58 +0000

    refactor(gax,wkt)!: rename GoogleCloudGax and GoogleCloudWKT to GoogleGax and GoogleWKT (#924)

--- Sources/GoogleAppsScriptTypeCalendar/CalendarAddOnManifest.swift
+++ Sources/GoogleAppsScriptTypeCalendar/CalendarAddOnManifest.swift
@@ -16,10 +16,10 @@
 
 import Foundation
 import GoogleAppsScriptType
-@_spi(GoogleCloudInternal) import GoogleCloudWKT
+@_spi(GoogleCloudInternal) import GoogleWKT
 
 /// Calendar add-on manifest.
-public struct CalendarAddOnManifest: Codable, Equatable, GoogleCloudWKT._AnyPackable,
+public struct CalendarAddOnManifest: Codable, Equatable, GoogleWKT._AnyPackable,
   Sendable
 {
   /// Defines an endpoint that will be executed contexts that don't
@@ -47,7 +47,7 @@ public struct CalendarAddOnManifest: Codable, Equatable, GoogleCloudWKT._AnyPack
   public var currentEventAccess: CalendarAddOnManifest.EventAccess =
     CalendarAddOnManifest.EventAccess()
 
-  @_spi(GoogleCloudInternal) public var _unknownFields: GoogleCloudWKT._UnknownFields = .init()
+  @_spi(GoogleCloudInternal) public var _unknownFields: GoogleWKT._UnknownFields = .init()
 
   /// Initialize a new instance of `CalendarAddOnManifest`.
   public init() {}
@@ -113,7 +113,7 @@ public struct CalendarAddOnManifest: Codable, Equatable, GoogleCloudWKT._AnyPack
     }
     for key in container.allKeys where !CodingKeys._knownKeys.contains(key.stringValue) {
       self._unknownFields.json[key.stringValue] = try container.decode(
-        GoogleCloudWKT.Value.self, forKey: key)
+        GoogleWKT.Value.self, forKey: key)
     }
   }
 
@@ -258,10 +258,10 @@ public struct CalendarAddOnManifest: Codable, Equatable, GoogleCloudWKT._AnyPack
   public static var _anyTypeUrl: Swift.String {
     return "type.googleapis.com/google.apps.script.type.calendar.CalendarAddOnManifest"
   }
-  public init(fromAny any: GoogleCloudWKT.`Any`) throws {
-    self = try GoogleCloudWKT._slowAnyDeserialize(Self.self, from: any)
+  public init(fromAny any: GoogleWKT.`Any`) throws {
+    self = try GoogleWKT._slowAnyDeserialize(Self.self, from: any)
   }
-  public func _pack() throws -> GoogleCloudWKT.Struct {
-    return try GoogleCloudWKT._slowAnySerialize(message: self)
+  public func _pack() throws -> GoogleWKT.Struct {
+    return try GoogleWKT._slowAnySerialize(message: self)
   }
 }
--- Sources/GoogleAppsScriptTypeCalendar/ConferenceSolution.swift
+++ Sources/GoogleAppsScriptTypeCalendar/ConferenceSolution.swift
@@ -15,10 +15,10 @@
 // limitations under the License.
 
 import Foundation
-@_spi(GoogleCloudInternal) import GoogleCloudWKT
+@_spi(GoogleCloudInternal) import GoogleWKT
 
 /// Defines conference related values.
-public struct ConferenceSolution: Codable, Equatable, GoogleCloudWKT._AnyPackable,
+public struct ConferenceSolution: Codable, Equatable, GoogleWKT._AnyPackable,
   Sendable
 {
   /// Required. The endpoint to call when ConferenceData should be created.
@@ -37,7 +37,7 @@ public struct ConferenceSolution: Codable, Equatable, GoogleCloudWKT._AnyPackabl
   /// Required. The URL for the logo image of the ConferenceSolution.
   public var logoUrl: Swift.String = Swift.String()
 
-  @_spi(GoogleCloudInternal) public var _unknownFields: GoogleCloudWKT._UnknownFields = .init()
+  @_spi(GoogleCloudInternal) public var _unknownFields: GoogleWKT._UnknownFields = .init()
 
   /// Initialize a new instance of `ConferenceSolution`.
   public init() {}
@@ -90,7 +90,7 @@ public struct ConferenceSolution: Codable, Equatable, GoogleCloudWKT._AnyPackabl
     }
     for key in container.allKeys where !CodingKeys._knownKeys.contains(key.stringValue) {
       self._unknownFields.json[key.stringValue] = try container.decode(
-        GoogleCloudWKT.Value.self, forKey: key)
+        GoogleWKT.Value.self, forKey: key)
     }
   }
 
@@ -108,10 +108,10 @@ public struct ConferenceSolution: Codable, Equatable, GoogleCloudWKT._AnyPackabl
   public static var _anyTypeUrl: Swift.String {
     return "type.googleapis.com/google.apps.script.type.calendar.ConferenceSolution"
   }
-  public init(fromAny any: GoogleCloudWKT.`Any`) throws {
-    self = try GoogleCloudWKT._slowAnyDeserialize(Self.self, from: any)
+  public init(fromAny any: GoogleWKT.`Any`) throws {
+    self = try GoogleWKT._slowAnyDeserialize(Self.self, from: any)
   }
-  public func _pack() throws -> GoogleCloudWKT.Struct {
-    return try GoogleCloudWKT._slowAnySerialize(message: self)
+  public func _pack() throws -> GoogleWKT.Struct {
+    return try GoogleWKT._slowAnySerialize(message: self)
   }
 }
